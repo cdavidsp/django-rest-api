d66833f9aa7dfaf3743922ddc52b2b930f7b5a75
Cesar Sosa <cesar.sosa@canary.is>
2022-06-30 14:59:59 +0200

    update refresh

--- schoolService/settings.py
+++ schoolService/settings.py
@@ -154,7 +154,11 @@ SWAGGER_SETTINGS = {
 
 
 SIMPLE_JWT = {
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
-    'ROTATE_REFRESH_TOKENS': True,
+
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False
 }
 
